7fbdac2818a52f5142ce101880bdf8b5fd66a37a
Edson <edsonrcb@hotmail.com>
2016-04-03 17:03:42 -0300

    Conectando main ao controller

--- src/br/univel/address/MainApp.java
+++ src/br/univel/address/MainApp.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 import br.univel.address.model.Person;
+import br.univel.address.view.PersonOverviewController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -80,9 +81,9 @@ public class MainApp extends Application {
         }
     }
 
-    /**
+/**
      * Mostra o person overview dentro do root layout.
-     */
+
     public void showPersonOverview() {
         try {
             // Carrega o person overview.
@@ -97,10 +98,34 @@ public class MainApp extends Application {
         }
     }
 
-    /**
+    
      * Retorna o palco principal.
      * @return
      */
+
+    /**
+     * Mostra a person overview dentro do root layout.
+     */
+    
+    public void showPersonOverview() {
+        try {
+            // Carrega a person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Define a person overview no centro do root layout.
+            rootLayout.setCenter(personOverview);
+
+            // Dá ao controlador acesso à the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     public Stage getPrimaryStage() {
         return primaryStage;
     }
