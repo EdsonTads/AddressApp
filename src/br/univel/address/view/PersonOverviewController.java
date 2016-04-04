36ca3e705d5f799747cea231e807be68cb87f687
Edson <edsonrcb@hotmail.com>
2016-04-03 23:46:08 -0300

    inicio parte 3

--- src/br/univel/address/view/PersonOverviewController.java
+++ src/br/univel/address/view/PersonOverviewController.java
@@ -48,29 +48,44 @@ public class PersonOverviewController {
         // Inicializa a tablea de pessoa com duas colunas.
         firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
         lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
-    }
+    
+     // Limpa os detalhes da pessoa.
+        showPersonDetails(null);
 
+        // Detecta mudanças de seleção e mostra os detalhes da pessoa quando houver mudança.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+        
+    /**
+     * É chamado pela aplicação principal para dar uma referência de volta a si mesmo.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Adiciona os dados da observable list na tabela
+        personTable.setItems(mainApp.getPersonData());
+    }
+    
     /**
      * PReenche todos os campos de texto para mostrar detalhes sobre a pessoa.
      * Se a pessoa especificada for null, todos os campos de texto são limpos.
      * 
      * @param person a pessoa ou null
      */
-
-    
  	private void showPersonDetails(Person person) {
 
- 		  birthdayLabel.setText(DateUtil.format(person.getBirthday()));
- 	}
- 	
-/**        if (person != null) {
+        if (person != null) {
             // Preenche as labels com informações do objeto person.
             firstNameLabel.setText(person.getFirstName());
             lastNameLabel.setText(person.getLastName());
             streetLabel.setText(person.getStreet());
             postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
             cityLabel.setText(person.getCity());
-
+            birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+            
             // TODO: Nós precisamos de uma maneira de converter o aniversário em um String! 
             // birthdayLabel.setText(...);
 
@@ -84,17 +99,13 @@ public class PersonOverviewController {
             birthdayLabel.setText("");
         }
     }
-*/
-    
-    /**
-     * É chamado pela aplicação principal para dar uma referência de volta a si mesmo.
-     * 
-     * @param mainApp
-     */
-    public void setMainApp(MainApp mainApp) {
-        this.mainApp = mainApp;
-
-        // Adiciona os dados da observable list na tabela
-        personTable.setItems(mainApp.getPersonData());
-    }
+ 	
+ 	/**
+ 	 * Chamado quando o usuário clica no botão delete.
+ 	 */
+ 	@FXML
+ 	private void handleDeletePerson() {
+ 	    int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+ 	    personTable.getItems().remove(selectedIndex);
+ 	}
 }
