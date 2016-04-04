16926ca0c74ea2235da253dd1f98512b2662ad04
Edson <edsonrcb@hotmail.com>
2016-04-04 19:25:13 -0300

    teste aula

--- src/br/univel/address/MainApp.java
+++ src/br/univel/address/MainApp.java
@@ -12,6 +12,7 @@ import br.univel.address.view.PersonEditDialogController;
 import br.univel.address.view.PersonOverviewController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -58,8 +59,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-     // Set the application icon.
-        this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Address_Book.png"));
 
         initRootLayout();
 
